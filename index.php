9193a04a605fa69d72e749773ca6284ecc426a0e
Maksim Kippa <maxthekeeper@gmail.com>
2016-02-19 23:39:29 +0300

    Подготовил каркас к выполнению первого дз

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP Level 1</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                
+                <a class="navbar-brand" href="/">
+                    <i class="fa fa-home"></i> PHP Level 1
+                </a>
+            </div>            
+        </div>
+    </nav>
+
+    <div class="container">
+        <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+            <li class="active"><a href="#lesson1" data-toggle="tab">Урок 1</a></li>
+            <li><a href="#orange" data-toggle="tab">Orange</a></li>
+            <li><a href="#yellow" data-toggle="tab">Yellow</a></li>
+            <li><a href="#green" data-toggle="tab">Green</a></li>
+            <li><a href="#blue" data-toggle="tab">Blue</a></li>
+        </ul>
+        <div id="my-tab-content" class="tab-content">
+            <div class="tab-pane active" id="lesson1">
+                <div class="row"><br>
+                    <div class="col-md-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Задание 1</div>
+
+                            <div class="panel-body">
+                                Тут будет текст задания.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Задание 1</div>
+
+                            <div class="panel-body">
+                                Тут будет текст задания.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Задание 1</div>
+
+                            <div class="panel-body">
+                                Тут будет текст задания.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane" id="orange">
+                <h1>Orange</h1>
+                <p>orange orange orange orange orange</p>
+            </div>
+            <div class="tab-pane" id="yellow">
+                <h1>Yellow</h1>
+                <p>yellow yellow yellow yellow yellow</p>
+            </div>
+            <div class="tab-pane" id="green">
+                <h1>Green</h1>
+                <p>green green green green green</p>
+            </div>
+            <div class="tab-pane" id="blue">
+                <h1>Blue</h1>
+                <p>blue blue blue blue blue</p>
+            </div>
+
+            <img class="logo" src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_logo.png" alt="The Source Logo">
+            <div id="ss_menu">
+              <div class="ss_button">1994</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_52/pages/1.jpg" alt="#52"></a>
+              </div>
+              <div class="ss_button">1995</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_64/pages/1.jpg" alt="#64"></a> <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_69/pages/1.jpg" alt="#69"></a>
+              </div>
+              <div class="ss_button">1996</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_79/pages/1.jpg" alt="#79"></a> <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_86/pages/1.jpg" alt="#86"></a>
+              </div>
+              <div class="ss_button">1997</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_91/pages/1.jpg" alt="#91"></a>
+              </div>
+              <div class="ss_button">1998</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_100/pages/1.jpg" alt="#100"></a>
+              </div>
+              <div class="ss_button">2000</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_135/pages/1.jpg" alt="#135"></a>
+              </div>
+              <div class="ss_button">2001</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_139/pages/1.jpg" alt="#139"></a>
+              </div>
+              <div class="ss_button">2002</div>
+              <div class="ss_content">
+                <a href="#"><img src="http://maxthekeeper.myjino.ru/uploads/magazines/the_source_148/pages/1.jpg" alt="#148"></a>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+    <!-- JavaScripts -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
