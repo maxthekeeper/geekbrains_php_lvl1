217600f3149202af4919eeae248285434f11dc43
maxthekeeper <maxthekeeper@gmail.com>
2016-03-04 15:48:26 +0300

    Набросал страничку с логином для первого задания 5 урока

---
+++ lesson5/index.php
@@ -0,0 +1,70 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Урок 5 / Задание 1</title>
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet">	
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+
+				<!-- Collapsed Hamburger -->
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<!-- Branding Image -->
+				
+				<a class="navbar-brand" href="index.php">
+					PHP Level 1
+				</a>
+			</div>
+			<div class="collapse navbar-collapse" id="app-navbar-collapse">
+				<!-- Left Side Of Navbar -->
+				<ul class="nav navbar-nav">
+					<li><a href="#">PHP Level 2</a></li>                    
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="panel panel-default">
+		            <div class="panel-heading">Login</div>
+		            <div class="panel-body">
+						
+						<!-- ФОРМА -->
+						<form class="form-horizontal" action="<?php echo htmlentities($_SERVER['SCRIPT_NAME']) ?>" method="POST">
+					        <div class="form-group">
+					        	<label class="col-sm-4 control-label" for="name">Ваше имя:</label>
+					        	<div class="col-sm-7">
+					        		<input type="text" name="name" id="name" class="form-control" value="">
+					        	</div>			            
+					        </div>
+					        <div class="form-group">
+					        	<div class="col-sm-offset-4 col-sm-8">
+					        		<button type="submit" class="btn btn-default ">Войти</button>
+					        	</div>			        	
+					        </div>			        
+					    </form>
+					</div>
+				</div>
+			</div>
+		</div>		
+	</div>
+</body>
+</html>
