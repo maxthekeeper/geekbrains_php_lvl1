e4a9815bb2b97c1af344ba23c53d3a2df6497bf7
Maksim Kippa <maxthekeeper@gmail.com>
2016-03-08 23:22:13 +0300

    Сделал галерею первого задания и накидал форму, доделать функционал формы загрузки файлов

--- lesson6/index.php
+++ lesson6/index.php
@@ -53,23 +53,15 @@
 											<!-- ФОРМА -->
 											<form class="form-horizontal" action="<?php echo htmlentities($_SERVER['SCRIPT_NAME']) ?>" method="POST">
 										        <div class="form-group">
-										        	<label class="col-sm-3 control-label" for="name">Ваш логин:</label>
+										        	<label class="col-sm-3 control-label" for="name">Выберите файл</label>
 										        	<div class="col-sm-8">
-										        		<input type="text" name="login" id="login" class="form-control" value="">
+										        		<input type="file" name="file" class="form-control">
+										        		<p class="help-block">Загрузите файл в формате .jpg</p>
 										        	</div>			            
 										        </div>
 										        <div class="form-group">
 										        	<div class="col-sm-offset-3 col-sm-6">
-										        		<div class="checkbox">
-												        	<label>
-												        		<input type="checkbox" name="rememberme"> Запомнить меня
-												        	</label>
-												        </div>
-										        	</div>					        	
-										        </div>					        
-										        <div class="form-group">
-										        	<div class="col-sm-offset-3 col-sm-6">
-										        		<button type="submit" class="btn btn-default ">Войти</button>
+										        		<button type="submit" class="btn btn-default ">Загрузить</button>
 										        	</div>			        	
 										        </div>
 										    </form>
@@ -79,6 +71,23 @@
 							</div>		
 						</div>
 						<hr>
+
+						<?php
+						$dir = "img/";
+
+						if (is_dir($dir)) {
+							if ($fd = opendir($dir)){
+								while (($file = readdir($fd)) !== false) {
+									if (substr($file, -4) == '.jpg') {				
+						?>
+										<a href="<?=$dir.$file;?>" target="_blank"><img src="<?=$dir.$file;?>" alt="" width="180"></a>
+						<?php
+									}
+								}
+							}
+						}
+						?>
+
 					</div>
 				</div>
 			</div>
